c8349b4d2a00d722af843b676b778590521c655b
Hugo <hugo.pigeon@yahoo.fr>
2014-04-07 11:06:52 +0200

    Ajout d'une barre de menu et des coordonnées

--- B editor/src/vue/Fenetre.java
+++ B editor/src/vue/Fenetre.java
@@ -1,9 +1,12 @@
 package vue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 /**
-* Classe Fenetre
+* Classe Fenetre : permet de gérer la fenêtre affichant l'éditeur de niveaux
 *
 * @author Hugo PIGEON
 * @version 1.0
@@ -11,12 +14,28 @@ import javax.swing.JFrame;
 
 public class Fenetre extends JFrame
 {	
+	private JMenuBar barreMenu;
+	private JMenu menuFichier;
+	private JMenuItem menuFichierQuitter;
+
+	/** Constructeur permettant de créer la fenêtre
+	*/
 	public Fenetre()
 	{
    		this.setTitle("B editor");
     	this.setSize(800, 600);
    		this.setLocationRelativeTo(null);
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+    	
+    	this.barreMenu = new JMenuBar();
+    	
+    	this.menuFichier = new JMenu("Fichier");
+    	this.barreMenu.add(this.menuFichier);
+    	this.menuFichierQuitter = new JMenuItem("Quitter");
+    	this.menuFichier.add(this.menuFichierQuitter);
+    	
+    	this.setJMenuBar(barreMenu);   
+    	  
     	this.setVisible(true);
 	}
 }
