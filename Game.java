0015d4c1a408f1028242ca1280ef18a7ef90be40
Quemard <mael.quemard@hotmail.fr>
2014-04-08 17:53:53 +0200

    MAJ game.java

--- Game.java
+++ Game.java
@@ -3,11 +3,19 @@ import model.*;
 
 class Game{
 
-	public static void main (String args[])
+	public void main
 	{
-		Player player = new Player;
+		Player player = new Player();
 		boolean isOpen=true;
-		String password
+		//Attributs pour la gestion FPS
+		final int TICKS_PER_SECOND = 50;
+		final int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
+		final int MAX_FRAMESKIP = 10;
+
+		static long next_game_tick = System.nanoTime();
+		long Dt; //le delta-t utilise par la gravite
+		int loops;
+		
 		//Effectuer les fonctionnalités du jeu tant qu'une fenêtre est ouverte
 		//Make the functionalities of the game features a window is opened
 		while(isOpen)
@@ -19,35 +27,35 @@ class Game{
 				Display.afficherMenu();
 				//Aller au menu crédit si le bouton crédit est appuyer
 				//Go to the menu credit if button credit is press
-				if (Display.buttonCredit.isClicked(souris)==true)
+				if (Display.buttonCredit.isClicked(mouse)==true)
 				{
 					Display.setMenu(false);
 					Display.setCredit(true);
 				}
 				//Aller au menu highscore si le bouton highscore est appuyer
 				//Go to the menu highscore if button highscore is press
-				else if (Display.buttonHighScore.isClicked(souris)==true)
+				else if (Display.buttonHighScore.isClicked(mouse)==true)
 				{
 					Display.setMenu(false);
 					Display.setHighScore(true);
 				}
 				//Aller au menu option si le bouton option est appuyer
 				//Go to the menu option if button option is press
-				else if (Display.buttonOption.isClicked(souris)==true)
+				else if (Display.buttonOption.isClicked(mouse)==true)
 				{
 					Display.setMenu(false);
 					Display.setOption(true);
 				}
 				//Aller au menu aide si le bouton aide est appuyer
 				//Go to the menu help if button help is press
-				else if (Display.buttonHelp.isClicked(souris)==true)
+				else if (Display.buttonHelp.isClicked(mouse)==true)
 				{
 					Display.setMenu(false);
 					Display.setHelp(true);
 				}
 				//Aller au menu choisir niveau si le bouton choisir niveau est appuyer
 				//Go to the menu choose level if button choose level is press
-				else if (Display.buttonChooseLevel.isClicked(souris)==true)
+				else if (Display.buttonChooseLevel.isClicked(mouse)==true)
 				{
 					Display.setMenu(false);
 					Display.setChooseLevel(true);
@@ -61,7 +69,7 @@ class Game{
 				
 				//Aller au menu si le bouton retour est appuyer
 				//Go to the menu if button return is press
-				if (Display.buttonReturn.isClicked(souris)==true)
+				if (Display.buttonReturn.isClicked(mouse)==true)
 				{
 					Display.setHighScore(false);
 					Display.setMenu(true);
@@ -75,7 +83,7 @@ class Game{
 				
 				//Aller au menu si le bouton retour est appuyer
 				//Go to the menu if button return is press
-				if (Display.buttonReturn.isClicked(souris)==true)
+				if (Display.buttonReturn.isClicked(mouse)==true)
 				{
 					Display.setCredit(false);
 					Display.setMenu(true);
@@ -89,7 +97,7 @@ class Game{
 				
 				//Aller au menu si le bouton retour est appuyer
 				//Go to the menu if button return is press
-				if (Display.buttonReturn.isClicked(souris)==true)
+				if (Display.buttonReturn.isClicked(mouse)==true)
 				{
 					Display.setHelp(false);
 					Display.setMenu(true);
@@ -102,14 +110,14 @@ class Game{
 				Display.afficherChooseLevel();
 				
 				//si le bouton du niveau 1 est appuyer alors jouer
-				if(Display.buttonsLevel[0].isClicked(souris))
+				if(Display.buttonsLevel[0].isClicked(mouse))
 				{
 					Level.play(1);
 				}
-				//du bouton du niveau 2 au dernier niveau incrémenter i et lancer le niveau correspondant au bouton (il y a le mot de passe a gerer encore)
+				//du bouton du niveau 2 au dernier niveau incrémenter i et lancer le niveau correspondant au bouton
 				for(int i=1; i<=nbLevel; i++)
 				{
-					if(Display.buttonsLevel[i].isClicked(souris)==true)
+					if(Display.buttonsLevel[i].isClicked(mouse)==true)
 					{
 						String password = Display.afficherDemandeDuMDP();
 						//si le mot de passe est bon
@@ -128,12 +136,12 @@ class Game{
 				
 				//Choix de la résolution full ou pixel (par box)
 				//Choose of size resolution (by box)
-				if(Display.buttonSizeResolutionFull.isClicked(souris))
+				if(Display.buttonSizeResolutionFull.isClicked(mouse))
 				{
 					Display.sizeResolutionFull();
 					ParametersResolutionFull();
 				}
-				else if(Display.buttonSizeResolutionPixel.isClicked(souris))
+				else if(Display.buttonSizeResolutionPixel.isClicked(mouse))
 				{
 					Display.sizeResolutionPixel();
 					Parameters.sizeResolutionPixel();
@@ -141,12 +149,12 @@ class Game{
 				
 				//Choix de la taille de l'écran (par box)
 				//Choose size of screen (by box)
-				if(Display.sizeScreenOne.isClicked(souris))
+				if(Display.sizeScreenOne.isClicked(mouse))
 				{
 					Display.sizeScreenOne();
 					Parameters.sizeScreenOne();
 				}
-				else if(Display.sizeScreenTwo.isClpicked(souris))
+				else if(Display.sizeScreenTwo.isClpicked(mouse))
 				{
 					Display.sizeScreenTwo();
 					Parameters.sizeScreenTwop();
@@ -154,12 +162,12 @@ class Game{
 				
 				//Choix des touches (par box)
 				//Choose key (by box)
-				if(Display.chooseKeyOne.isClicked(souris)
+				if(Display.chooseKeyOne.isClicked(mouse))
 				{
 					Display.chooseKeyOne();
 					Parameters.chooseKeyOne();
 				}
-				else if(Display.chooseKeyTwo.isClicked(souris))
+				else if(Display.chooseKeyTwo.isClicked(mouse))
 				{
 					Display.chooseKeyTwo();
 					Parameters.chooseKeyTwo();
@@ -167,18 +175,18 @@ class Game{
 				
 				//Choix de la texture (par box)
 				//Choose of texture (by box)
-				if(Display.textureOne.isClicked(souris))
+				if(Display.textureOne.isClicked(mouse))
 				{
 					Display.textureOne();
 					Parameters.textureOne();
 				}
-				else if(Display.textureTwo.isClicked(souris))
+				else if(Display.textureTwo.isClicked(mouse))
 				{
 					Display.textureTwo();
 					Parameters.textureTwo();
 				}
 				
-				if(Display.curseurVolM.isClicked(souris))
+				if(Display.curseurVolM.isClicked(mouse))
 				{
 					switch(Display.curseur)
 					{
@@ -193,9 +201,15 @@ class Game{
 						case 50:
 							Display.curseur(50);
 							Parameters.musique(50);
+						break;
 						case 25:
 							Display.curseur(25);
 							Parameters.musique(25);
+						break;
+						case 0:
+							Display.curseur(0);
+							Parameters.musique(0);
+						break;
 					}
 				}
 			}
@@ -213,9 +227,24 @@ class Game{
 					{
 					
 					}
+					else
+					{
+						//Fonction pour le régulateur de la vitesse du jeu
+						//Le jeu sera mis a jour environ 50 fois par seconde, et le rendu sera fait aussi vite que possible
+						loops = 0;
+						Dt = System.nanoTime()
+						while(System.nanoTime() > next_game_tick && loops < MAX_FRAMESKIP)
+						{
+								Dt = System.nanoTime() - Dt;
+								update_game(Dt);
+
+								next_game_tick += SKIP_TICKS;
+								loops++;
+						}
+						display_game();
+					}
 				}
 			}
-			
 		}
 	}
 }
